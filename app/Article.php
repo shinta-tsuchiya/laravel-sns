fca1217393c62454dab7398e95d5178c17848d1b
shintrb <shintrb@shintrbnoMacBook-Air.local>
2020-05-13 16:13:36 +0900

    1-6 complete

---
+++ app/Article.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Article extends Model // Articleモデルは Modelクラスを継承している
+{
+    public function user(): BelongsTo
+    // :BelongsTo メソッドの戻り値の型を宣言
+    // PHP7では関数・メソッドの戻り値の型宣言が使える
+    // 型:整数、文字列、配列、オブジェクト、クラスなど
+    // ここではuserメソッドの戻り値が BelongsTo クラスであることを宣言
+    // もし他の型を返そうとした時点でTypeError 例外が発生して処理を終了する
+    {
+        return $this->belongsTo('App\User');
+        // $thisはArticleクラスのインスタンス自身を指す
+        // $this->メソッド名() インスタンスが持つメソッドが実行され、
+        // $this->プロパティ名 インスタンスが持つプロパティを参照する
+        // リレーション
+    }
+}
