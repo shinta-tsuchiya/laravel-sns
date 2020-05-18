0bd9c7306721679b6b2f1a262ad7321e7760311b
shintrb <tsuchiyashinta@gmail.com>
2020-05-18 16:06:59 +0900

    5-4 complete

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -46,4 +46,20 @@ class ArticleController extends Controller
         return view('articles.edit', ['article' => $article]);    
     }
     //==========ここまで追加==========
+
+    //==========ここから追加==========
+    public function update(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all())->save();
+        return redirect()->route('articles.index');
+    }
+    //==========ここまで追加==========
+
+    //==========ここから追加==========
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return redirect()->route('articles.index');
+    }
+    //==========ここまで追加==========
 }
