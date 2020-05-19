e6a56700df4d7040ddeab5b269d732b3c42b5184
shintrb <tsuchiyashinta@gmail.com>
2020-05-19 22:09:43 +0900

    8-2 complete

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,11 +2,12 @@
 import './bootstrap'
 import Vue from 'vue'
 import ArticleLike from './components/ArticleLike'
+import ArticleTagsInput from './components/ArticleTagsInput'
 
 const app = new Vue({
   el: '#app',
   components: {
     ArticleLike,
+    ArticleTagsInput,
   }
 })
-//==========ここまで追加==========
