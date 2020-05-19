770267779ff8c04f4428297bd0e3cf4c2baa1218
shintrb <tsuchiyashinta@gmail.com>
2020-05-19 23:32:01 +0900

    8-11 complete

--- app/Tag.php
+++ app/Tag.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
 class Tag extends Model
 {
     protected $fillable = [
@@ -12,5 +14,10 @@ class Tag extends Model
     public function getHashtagAttribute(): string
     {
         return '#' . $this->name;
+    }
+
+    public function articles(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Article')->withTimestamps();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,8 @@ Auth::routes();
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth'); //-- この行を編集
 Route::resource('/articles', 'ArticleController')->only(['show']);
-//==========ここから追加==========
 Route::prefix('articles')->name('articles.')->group(function () {
     Route::put('/{article}/like', 'ArticleController@like')->name('like')->middleware('auth');
     Route::delete('/{article}/like', 'ArticleController@unlike')->name('unlike')->middleware('auth');
 });
-//==========ここまで追加==========
+Route::get('/tags/{name}', 'TagController@show')->name('tags.show');
