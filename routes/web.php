707dc3233a0644ac4fc597e99ebfefad91920589
shintrb <tsuchiyashinta@gmail.com>
2020-05-20 23:29:04 +0900

    10-7 complete

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::prefix('login')->name('login.')->group(function () {
 });
 Route::prefix('register')->name('register.')->group(function () {
     Route::get('/{provider}', 'Auth\RegisterController@showProviderUserRegistrationForm')->name('{provider}');
+    Route::post('/{provider}', 'Auth\RegisterController@registerProviderUser');
 });
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth'); //-- この行を編集
