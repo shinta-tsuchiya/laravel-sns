1df1eef54eecad625d3da313f8e064a3c7941bb3
shintrb <tsuchiyashinta@gmail.com>
2020-05-20 22:28:22 +0900

    10-4 complete

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,52 +3,56 @@
 @section('title', 'ログイン')
 
 @section('content')
-  <div class="container">
-    <div class="row">
-      <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
-        <h1 class="text-center"><a class="text-dark" href="/">memo</a></h1>
-        <div class="card mt-3">
-          <div class="card-body text-center">
-            <h2 class="h3 card-title text-center mt-2">ログイン</h2>
+<div class="container">
+  <div class="row">
+    <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
+      <h1 class="text-center"><a class="text-dark" href="/">memo</a></h1>
+      <div class="card mt-3">
+        <div class="card-body text-center">
+          <h2 class="h3 card-title text-center mt-2">ログイン</h2>
 
-            @include('error_card_list')
-            
-            <div class="card-text">
-              <form method="POST" action="{{ route('login') }}">
-                @csrf
+          <a href="{{ route('login.{provider}', ['provider' => 'google']) }}" class="btn btn-block btn-danger">
+            <i class="fab fa-google mr-1"></i>Googleでログイン
+          </a>
 
-                <div class="md-form">
-                  <label for="email">メールアドレス</label>
-                  <input class="form-control" type="text" id="email" name="email" required value="{{ old('email') }}">
-                </div>
+          @include('error_card_list')
 
-                <div class="md-form">
-                  <label for="password">パスワード</label>
-                  <input class="form-control" type="password" id="password" name="password" required>
-                </div>
- 
-                {{--ここから--}}
-                <input type="hidden" name="remember" id="remember" value="on">
-                {{--ここまで--}}
-                
-                {{-- ここから追加 --}}
-                <div class="text-left">
-                  <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
-                </div>
-                {{-- ここまで追加 --}}
+          <div class="card-text">
+            <form method="POST" action="{{ route('login') }}">
+              @csrf
 
-                <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
-
-              </form>
-
-              <div class="mt-0">
-                <a href="{{ route('register') }}" class="card-text">ユーザー登録はこちら</a>
+              <div class="md-form">
+                <label for="email">メールアドレス</label>
+                <input class="form-control" type="text" id="email" name="email" required value="{{ old('email') }}">
               </div>
 
+              <div class="md-form">
+                <label for="password">パスワード</label>
+                <input class="form-control" type="password" id="password" name="password" required>
+              </div>
+
+              {{--ここから--}}
+              <input type="hidden" name="remember" id="remember" value="on">
+              {{--ここまで--}}
+
+              {{-- ここから追加 --}}
+              <div class="text-left">
+                <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
+              </div>
+              {{-- ここまで追加 --}}
+
+              <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
+
+            </form>
+
+            <div class="mt-0">
+              <a href="{{ route('register') }}" class="card-text">ユーザー登録はこちら</a>
             </div>
+
           </div>
         </div>
       </div>
     </div>
   </div>
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 Auth::routes();
+Route::prefix('login')->name('login.')->group(function () {
+    Route::get('/{provider}', 'Auth\LoginController@redirectToProvider')->name('{provider}');
+});
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth'); //-- この行を編集
 Route::resource('/articles', 'ArticleController')->only(['show']);
