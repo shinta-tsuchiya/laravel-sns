7104b502115b2bc38de573a45fbe9537d7bba3c9
shintrb <tsuchiyashinta@gmail.com>
2020-07-08 15:46:30 +0900

    composer update

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -6,8 +6,8 @@
 
     @guest {{--@guestから@endguestに囲まれた部分は、ユーザーがまだログインしていない状態の時のみ処理される--}}
     <li class="nav-item">
-    <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
-    {{--route関数を使って、ユーザー登録画面への遷移を可能にしている--}}
+      <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
+      {{--route関数を使って、ユーザー登録画面への遷移を可能にしている--}}
     </li>
     @endguest
 
@@ -19,19 +19,20 @@
 
     @auth {{--@authから@endauthに囲まれた部分は、ユーザーがログイン済みの状態の時のみ処理--}}
     <li class="nav-item">
-      <a class="nav-link" href="{{ route('articles.create') }}"><i class="fas fa-pen mr-1"></i>投稿する</a> {{--この行のhref属性を変更--}}    </li>
+      <a class="nav-link" href="{{ route('articles.create') }}"><i class="fas fa-pen mr-1"></i>投稿する</a>
+      {{--この行のhref属性を変更--}} </li>
     @endauth
 
     @auth
     <!-- Dropdown -->
     <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
-         aria-haspopup="true" aria-expanded="false">
+      <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+        aria-expanded="false">
         <i class="fas fa-user-circle"></i>
       </a>
       <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
         <button class="dropdown-item" type="button"
-        onclick="location.href='{{ route("users.show", ["name" => Auth::user()->name]) }}'">
+          onclick="location.href='{{ route("users.show", ["name" => Auth::user()->name]) }}'">
           マイページ
         </button>
         <div class="dropdown-divider"></div>
@@ -42,7 +43,7 @@
       </div>
     </li>
     <form id="logout-button" method="POST" action="{{ route('logout') }}">
-    {{--HTTPメソッドはPOSTである必要があるので、aタグではなくbuttonタグとformタグを使う--}}
+      {{--HTTPメソッドはPOSTである必要があるので、aタグではなくbuttonタグとformタグを使う--}}
       @csrf
     </form>
     <!-- Dropdown -->
